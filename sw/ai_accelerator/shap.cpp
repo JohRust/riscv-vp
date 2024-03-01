6aeb8756a000a3ea51b0c788c5435cb24f609b63
Johannes Rust <johannes.rust@dfki.de>
2024-03-01 19:07:49 +0100

    Adapt explainPrediction to new functions

--- sw/ai_accelerator/shap.cpp
+++ sw/ai_accelerator/shap.cpp
@@ -37,53 +37,55 @@ uint64_t factorial(uint32_t n) {
 	return res;
 }
 
-/**
- * Calculates the frequency of feature i in the Shapley value calculation.
- *
- * @param n The total number of features.
- * @param s The number of features in the subset.
- * @return The frequency of feature i in the Shapley value calculation.
- */
 float shapley_frequency(uint32_t n, uint32_t s) {
 	// Calculate the frequency of feature i in the shapley value calculation
 	return n / binomialCoeff(n, s);
 }
 
-float marginalContribution(const std::vector<float>& input_data, const std::vector<bool>& mask) {
-	// Calculate the marginal contribution of feature i
-	std::vector<float> data_masked(input_data);
-	replaceValues(data_masked, mask, input_data);
-	return (req_prediction(&input_data[0], input_data.size()) - req_prediction(&data_masked[0], data_masked.size()));
-}
-
-// TODO: Extract requests and make
-void explainPrediction(const std::vector<float>& input_data, std::vector<float>& shapley_values) {
+void explainPrediction(std::vector<float> input_data, std::vector<float>& shapley_values) {
 	// Calculate the shapley value of every value in input_data
 	uint32_t n = input_data.size();
 	for (uint32_t i = 0; i < n; ++i) {
 		shapley_values[i] = 0;
 	}
-	for (uint32_t i = 0; i < n; ++i) {  // Shapley value for feature i
-		std::vector<bool> mask(n, false);
-		mask[i] = true;
-		for (uint32_t j = 0; j < n; ++j) {
-			if (j != i) {
-				mask[j] = true;
-				uint32_t numMasked = 0;
-				for (uint32_t k = 0; k < n; ++k) {
-					if (mask[k]) {
-						numMasked++;
-					}
-				}
-				std::vector<float> data_masked(input_data);
-				replaceValues(data_masked, mask, input_data);
 
-				float marginalContribution = (req_prediction(&input_data[0], input_data.size()) -
-				                              req_prediction(&data_masked[0], data_masked.size())) /
-				                             binomialCoeff(n - 1, numMasked);
-				shapley_values[i] += marginalContribution;
-				mask[j] = false;
+	std::vector<bool> mask(n, false);
+	for (uint32_t i = 0; i < n; ++i) {  // Shapley value for feature i
+		mask[i] = true;
+		uint64_t num_subsets = 1 << (n - 1);  // 2^(n-1) subsets of N\{i}
+		for (uint32_t j = 0; j < num_subsets; ++j) {
+			if (j == i) {
+				continue;  // Exclude the feature itself
 			}
+
+			auto mask = getAsBoolVector(j, n - 1);
+			mask.insert(mask.begin() + i, false);  // Add the feature back in
+			                                       // We need to know the size of the subset called |S|
+			uint32_t numMasked = 0;
+			for (uint32_t k = 0; k < n; ++k) {
+				if (mask[k]) {
+					numMasked++;
+				}
+			}
+			std::vector<float> data_masked(input_data);
+			replaceValues(data_masked, mask, input_data);
+
+			input_data[i] = 1;
+			float pred_with_i = req_prediction(&data_masked[0], data_masked.size());
+			input_data[i] = 0;
+			float pred_without_i = req_prediction(&input_data[0], input_data.size());
+
+			shapley_values[i] += shapley_frequency(n, numMasked) * (pred_with_i - pred_without_i);
 		}
 	}
+}
+
+std::vector<bool> getAsBoolVector(uint32_t n, uint16_t length) {
+	std::vector<bool> res(length, false);
+	for (uint16_t i = 0; i < length; ++i) {
+		if (n & (1 << i)) {
+			res[i] = true;
+		}
+	}
+	return res;
 }
--- sw/ai_accelerator/shap.h
+++ sw/ai_accelerator/shap.h
@@ -1,9 +1,59 @@
 #include <vector>
 #include <cstdint>
 
+/**
+ * Replaces values in the given array based on the provided mask and new values.
+ *
+ * @param array The array of float values to be modified.
+ * @param mask The mask indicating which values in the array should be replaced.
+ * @param newValues The new values to replace the masked values in the array.
+ */
 void replaceValues(std::vector<float>& array, const std::vector<bool>& mask, const std::vector<float>& newValues);
 
+/**
+ * Calculates the binomial coefficient.
+ *
+ * @param n The total number of elements.
+ * @param k The number of elements in the subset.
+ * @return The binomial coefficient.
+ */
 uint32_t binomialCoeff(uint32_t n, uint32_t k);
 
-// TODO: Extract requests and make 
-void explainPrediction(const std::vector<float>& input_data, std::vector<float>& shapley_values);
+/**
+ * Calculates the frequency of feature i in the Shapley value calculation.
+ *
+ * @param n The total number of features.
+ * @param s The number of features in the subset.
+ * @return The frequency of feature i in the Shapley value calculation.
+ */
+float shapley_frequency(uint32_t n, uint32_t s);
+
+/**
+ * Calculates the marginal contribution of a feature in the input data based on a given mask.
+ * 
+ * @param input_data The input data vector.
+ * @param mask The mask vector indicating which elements of the input data should be masked.
+ * @return The marginal contribution of the feature.
+ */
+float marginalContribution(const std::vector<float>& input_data, const std::vector<bool>& mask);
+
+/**
+ * Explains the prediction of the model by calculating the Shapley value of every value in the input data.
+ * 
+ * @param input_data The input data vector.
+ * @param shapley_values The vector to store the Shapley values in.
+ */
+void explainPrediction(std::vector<float> input_data, std::vector<float>& shapley_values);
+
+/**
+ * Get a binary representation of n as a vector of bools.
+ * E.g. getAsBoolVector(5, 3) -> 101 -> {true, false, true}
+ * This is an easy way to get masks for all subsets of a set.
+ * If you have a set of n elements, there are 2^n subsets.
+ * So calling this function with 0...(n-1) will give you all subsets.
+ *
+ * @param n The number to convert to a binary representation.
+ * @param length The length of the binary representation.
+ * @return A vector of bools representing the binary representation of n.
+ */
+std::vector<bool> getAsBoolVector(uint32_t n, uint16_t length);
