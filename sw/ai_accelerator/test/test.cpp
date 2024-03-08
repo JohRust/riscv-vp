ec3ca7fc6b268d70a424f9c3bef5fe232138f3dc
Johannes Rust <johannes.rust@dfki.de>
2024-03-08 16:53:04 +0100

    More tests

--- sw/ai_accelerator/test/test.cpp
+++ sw/ai_accelerator/test/test.cpp
@@ -3,6 +3,24 @@
 #include <gtest/gtest.h>
 #include "../src/shap.h"
 
+TEST(getAsBoolVector, getAsBoolVectorTest) {
+    EXPECT_EQ(getAsBoolVector(5, 5), std::vector<bool>({true, false, true, false, false}));
+    EXPECT_EQ(getAsBoolVector(6, 5), std::vector<bool>({false, true, true, false, false}));
+    EXPECT_EQ(getAsBoolVector(7, 5), std::vector<bool>({true , true, true, false, false}));
+}
+
+TEST(replaceValues, replaceValuesTest) {
+    std::vector<float> array = {1, 2, 3, 4, 5};
+    std::vector<bool> mask = {true, false, true, false, true};
+    std::vector<float> newValues = {10, 20, 30, 40, 50};
+    replaceValues(array, mask, newValues);
+    EXPECT_EQ(array[0], 10);
+    EXPECT_EQ(array[1], 2);
+    EXPECT_EQ(array[2], 30);
+    EXPECT_EQ(array[3], 4);
+    EXPECT_EQ(array[4], 50);
+}
+
 // Test the binomial coefficient function
 TEST(binomialCoeff, bonimialCoeffTest) {
     EXPECT_EQ(binomialCoeff(5, 2), 10);
@@ -19,15 +37,15 @@ TEST(factorial, factorialTest) {
     EXPECT_EQ(factorial(8), 40320);
     EXPECT_EQ(factorial(0), 1);
     EXPECT_EQ(factorial(20), 2432902008176640000);
-    EXPECT_EQ(factorial(21), -1);
-    EXPECT_EQ(factorial(-1), -1);
+    EXPECT_EQ(factorial(21), -1); // Avoid overflow
+    EXPECT_EQ(factorial(-1), -1); // Negative values are invalid input
 }
 
 TEST(shapley_frequency, shapleyFrequencyTest) {
     EXPECT_FLOAT_EQ(shapley_frequency(3, 0), 0.333333343);
     EXPECT_FLOAT_EQ(shapley_frequency(3, 1), 0.1666667);
     EXPECT_FLOAT_EQ(shapley_frequency(3, 2), 0.333333343);
-    EXPECT_FLOAT_EQ(shapley_frequency(3, 3), 0.0);
+    EXPECT_FLOAT_EQ(shapley_frequency(3, 3), 0.0); // Special case to avoid undefined behavior 
 }
 
 int main(int argc, char* argv[]) {
