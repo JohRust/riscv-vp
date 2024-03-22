db0f9930b2293cf44e24a027b22f93d51275ca4d
Johannes Rust <johannes.rust@dfki.de>
2024-03-22 13:56:13 +0100

    fix makefile for risc-vp

--- sw/ai_accelerator/src/main.cpp
+++ sw/ai_accelerator/src/main.cpp
@@ -1,10 +1,8 @@
 #include <vector>
-
-#include "ai_caller.h"
-#include "shap.h"
 #include "stdio.h"
 #include "unistd.h"
-
+#include "shap.h"
+#include "ai_caller.h"
 volatile int was_itr_triggered = 0;
 
 // TODO: Call this
@@ -19,6 +17,9 @@ int main(int argc, char **argv) {
 	std::vector<std::vector<float>> background_data = {
 		{0.0, 0.5, 1.0, 1.5}, {1.0, 1.5, 2.0, 2.5}, {2.0, 2.5, 3.0, 3.5}};
 	std::vector<float> shapley_values = explainPrediction(input_data, reqPrediction, background_data);
-
+	printf("Shapley values: ");
+	for (auto value : shapley_values) {
+		printf("%f ", value);
+	}
 	return 0;
 }
