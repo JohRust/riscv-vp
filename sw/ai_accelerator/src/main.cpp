5b2fdbd2fd44f3eeba603541865ece489f2a6468
Johannes Rust <johannes.rust@dfki.de>
2024-03-22 17:36:06 +0100

    Minor changes

--- sw/ai_accelerator/src/main.cpp
+++ sw/ai_accelerator/src/main.cpp
@@ -3,7 +3,16 @@
 #include "unistd.h"
 #include "shap.h"
 #include "ai_caller.h"
+extern "C" {
+	#include "irq.h"
+}
 volatile int was_itr_triggered = 0;
+
+volatile bool has_sensor_data = 0;
+
+void sensor_irq_handler() {
+	has_sensor_data = 1;
+}
 
 // TODO: Call this
 // void init() {
@@ -11,8 +20,7 @@ volatile int was_itr_triggered = 0;
 //}
 
 int main(int argc, char **argv) {
-	int n = printf("Running Linear Regression\n");
-
+	register_interrupt_handler(2, sensor_irq_handler);
 	std::vector<float> input_data = {1.0, 2.0, 3.0, 4.0};
 	std::vector<std::vector<float>> background_data = {
 		{0.0, 0.5, 1.0, 1.5}, {1.0, 1.5, 2.0, 2.5}, {2.0, 2.5, 3.0, 3.5}};
