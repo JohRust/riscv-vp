a1ced23e51d1560b009976e6b0ae59b92483f041
Johannes Rust <rust.johannes@web.de>
2024-06-26 11:57:23 +0200

    enum for custom syscalls. Also add exit syscall in call.S to let program exit gracefully.

--- sw/custom_syscall/main.cpp
+++ sw/custom_syscall/main.cpp
@@ -3,9 +3,15 @@
 
 extern "C" int make_syscall(int arg1, int arg2, int syscall_number);
 
+enum my_syscalls {
+    SET_TRACES = 700,
+    CUSTOM_SYSCALL1 = 701,
+    CUSTOM_SYSCALL2 = 702,
+};
+
 int main(){
-    //make syscall
-    int res = make_syscall(5, 3, 701);
+    //make a syscall
+    int res = make_syscall(5, 3, CUSTOM_SYSCALL1);
     printf("syscall result: %d\n", res);
     return 0;
 }
